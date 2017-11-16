18fd8ff50ecc153acaff75786452829ec148e246
전상현 <ali.jeon@kakaobank.com>
2017-11-16 17:52:14 +0900

    update

--- lib/fast_rcnn/config.py
+++ lib/fast_rcnn/config.py
@@ -35,7 +35,7 @@ cfg = __C
 __C.TRAIN = edict()
 #__C.NET_NAME = 'VGGnet'
 # learning rate
-__C.TRAIN.LEARNING_RATE = 0.0001
+__C.TRAIN.LEARNING_RATE = 0.001
 __C.TRAIN.MOMENTUM = 0.9
 __C.TRAIN.GAMMA = 0.1
 __C.TRAIN.STEPSIZE = 50000
